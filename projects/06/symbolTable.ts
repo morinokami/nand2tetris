128aa1125ed978636b8c1c216b39fbe14da746bd
Shinya Fujino <shf0811@gmail.com>
2021-08-08 13:19:32 +0900

    SymbolTable

--- projects/06/symbolTable.ts
+++ projects/06/symbolTable.ts
@@ -7,17 +7,23 @@ class SymbolTable {
   /**
    * Adds <symbol, address> to the table.
    */
-  addEntry(symbol: string, address: number): void {}
+  addEntry(symbol: string, address: number): void {
+    this.table.set(symbol, address);
+  }
 
   /**
    * Does the symbol table contain the given symbol?
    */
-  contains(symbol: string): boolean {}
+  contains(symbol: string): boolean {
+    return this.table.has(symbol);
+  }
 
   /**
    * Returns the address associated with the symbol.
    */
-  getAddress(symbol: string): number {}
+  getAddress(symbol: string): number | undefined {
+    return this.table.get(symbol);
+  }
 }
 
 export default SymbolTable;
