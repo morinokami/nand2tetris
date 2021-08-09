d0f6980e2d8ae68b1afcd0863d3772acb992edc0
Shinya Fujino <shf0811@gmail.com>
2021-08-09 18:41:40 +0900

    Add assemble function

--- projects/06/hackAssembler.ts
+++ projects/06/hackAssembler.ts
@@ -12,48 +12,53 @@ function toBinary(value: string, length = 16): string {
   return result;
 }
 
+function assemble(program: string): string {
+  const parser = new Parser(program);
+
+  // First pass: initialize the symbol table
+  const symbolTable = new SymbolTable();
+  while (parser.hasMoreLines()) {
+    const instructionType = parser.instructionType();
+    if (instructionType === Instruction.L) {
+      const symbol = parser.symbol();
+      symbolTable.addEntry(symbol, parser.lineNumber);
+      parser.lineNumber--;
+    }
+    parser.advance();
+  }
+  parser.reset();
+
+  // Second pass: translate the program into Hack binary code
+  let variableIndex = 16;
+  const result: string[] = [];
+  while (parser.hasMoreLines()) {
+    const instructionType = parser.instructionType();
+    if (instructionType === Instruction.A) {
+      let address = parser.symbol();
+      if (isNaN(parseInt(address))) {
+        // encountered a symbolic reference
+        const symbol = address;
+        if (!symbolTable.contains(symbol)) {
+          symbolTable.addEntry(symbol, variableIndex);
+          variableIndex++;
+        }
+        address = String(symbolTable.getAddress(symbol) as number);
+      }
+      result.push(toBinary(address));
+    } else if (instructionType === Instruction.C) {
+      const dest = parser.dest();
+      const comp = parser.comp();
+      const jump = parser.jump();
+      result.push(`111${Code.comp(comp)}${Code.dest(dest)}${Code.jump(jump)}`);
+    }
+    parser.advance();
+  }
+
+  return result.join("\n");
+}
+
 const filename = Deno.args[0];
 const filenameWithoutExtension = path.parse(filename).name;
 const program = await Deno.readTextFile(filename);
-const parser = new Parser(program);
-
-// First pass: initialize the symbol table
-const symbolTable = new SymbolTable();
-while (parser.hasMoreLines()) {
-  const instructionType = parser.instructionType();
-  if (instructionType === Instruction.L) {
-    const symbol = parser.symbol();
-    symbolTable.addEntry(symbol, parser.lineNumber);
-    parser.lineNumber--;
-  }
-  parser.advance();
-}
-parser.reset();
-
-// Second pass: translate the program into Hack binary code
-let variableIndex = 16;
-const result: string[] = [];
-while (parser.hasMoreLines()) {
-  const instructionType = parser.instructionType();
-  if (instructionType === Instruction.A) {
-    let address = parser.symbol();
-    if (isNaN(parseInt(address))) {
-      // encountered a symbolic reference
-      const symbol = address;
-      if (!symbolTable.contains(symbol)) {
-        symbolTable.addEntry(symbol, variableIndex);
-        variableIndex++;
-      }
-      address = String(symbolTable.getAddress(symbol) as number);
-    }
-    result.push(toBinary(address));
-  } else if (instructionType === Instruction.C) {
-    const dest = parser.dest();
-    const comp = parser.comp();
-    const jump = parser.jump();
-    result.push(`111${Code.comp(comp)}${Code.dest(dest)}${Code.jump(jump)}`);
-  }
-  parser.advance();
-}
-
-await Deno.writeTextFile(`${filenameWithoutExtension}.hack`, result.join("\n"));
+const result = assemble(program);
+await Deno.writeTextFile(`${filenameWithoutExtension}.hack`, result);
